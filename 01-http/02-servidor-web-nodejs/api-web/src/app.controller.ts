2e118d281066670de7d5f0d66df5714bc2f3c35b
Pame1995 <pamela_pachacama@hotmail.com>
2019-04-10 16:02:07 -0500

    metodo post

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,29 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post() //Metodo Http
+  @HttpCode(200)
+  postHello(){
+    return 'hola mundo en post';
+  }
+  /**@NombreDecorador() //Decorador-> funcion
+  class usuario{
+    @atributoPublico; //atributo
+    private atributoPrivado;
+    protected atributoProtegido;
+
+    constructor(@Parametro atributoPublico, atributoPrivado,atributoProtegido){
+      this.atributoPublico=atributoPublico;
+      this.atributoPrivado=atributoPrivado;
+      this.atributoProtegido=atributoProtegido;
+    }
+    @MetodoA()
+    public metodoPublico(){}
+
+  @MetodoB()
+  private metodoPrivado(){}
+    protected metdoProtegifo(){}
+
+**/
 }
