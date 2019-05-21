235d9ac69fa3e9c8afeb3aa8eadc4ea01c6b1b89
Pame1995 <pamela_pachacama@hotmail.com>
2019-05-21 02:41:00 -0500

    pag-web-w3school

--- Deberes/03-paginaWeb/pag-web/src/app.controller.ts
+++ Deberes/03-paginaWeb/pag-web/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,Response } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/pagWeb')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('inicio')
+  peliculas(@Response() res){
+    return res.render('inicio',{
+
+    });
   }
 }
