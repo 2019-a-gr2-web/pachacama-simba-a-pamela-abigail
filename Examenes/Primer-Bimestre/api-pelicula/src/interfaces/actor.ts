49a7c8c3df381d9d0314a0fef5a65600cec48383
Pame1995 <pamela_pachacama@hotmail.com>
2019-06-03 00:01:26 -0500

    examen

---
+++ Examenes/Primer-Bimestre/api-pelicula/src/interfaces/actor.ts
@@ -0,0 +1,8 @@
+export interface Actor {
+    idActor?: number;
+    nombre: string;
+    apellido: string;
+    fechaNacimiento: Date;
+    numeroPeliculas: number;
+    retirado: boolean;
+}
---
+++ Examenes/Primer-Bimestre/api-pelicula/src/interfaces/pelicula.ts
@@ -0,0 +1,9 @@
+export interface Peliculas {
+    idPelicula?: number;
+    nombre: string;
+    anioLanzamineto: number;
+    rating: number ;
+    actoresPrincipales: string;
+    sinopsis: string;
+    idActor?: number;
+}
